b07cc5a60541d4d43c463501f502f973a225cb01
zleoliu <zleoliu@tencent.com>
2023-03-11 20:29:16 +0800

    feat(all): implement random terrian generation

--- Minecraft/Source/Minecraft/Chunk/Chunk.cpp
+++ Minecraft/Source/Minecraft/Chunk/Chunk.cpp
@@ -1,17 +1,19 @@
 ﻿#include "Chunk.h"
+
 #include "Minecraft/World/WorldConstants.h"
+#include "Minecraft/WorldGenerator/WorldGenerator.h"
 
 Chunk::Chunk(int x, int y)
 {
 	size = WorldConstants::CHUNK_ELEMENT_COUNT;
 	depth = WorldConstants::CHUNK_DEPTH;
 	blockSize = WorldConstants::UNIT_BLOCK_SIZE;
-	chunkSize = size*blockSize;
-	
+	chunkSize = size * blockSize;
+
 	indexX = x;
 	indexY = y;
 
-	origin = FVector((indexX-0.5)*chunkSize, (indexY-0.5)*chunkSize, 0);
+	origin = FVector((indexX - 0.5) * chunkSize, (indexY - 0.5) * chunkSize, 0);
 	Blocks = std::vector<std::vector<std::vector<BlockEnumType>>>();
 	Blocks.resize(size, std::vector<std::vector<BlockEnumType>>(
 		              size, std::vector<BlockEnumType>(depth)));
@@ -22,7 +24,7 @@ Chunk::Chunk(int x, int y)
 
 	BlocksInstanceIndex = std::vector<std::vector<std::vector<int>>>();
 	BlocksInstanceIndex.resize(size, std::vector<std::vector<int>>(
-								   size, std::vector<int>(depth, -1)));
+		                           size, std::vector<int>(depth, -1)));
 	InitBlocks();
 }
 
@@ -38,22 +40,13 @@ void Chunk::GenerateTerrain()
 	{
 		for (int y = 0; y < size; y++)
 		{
-			int height =1;
+			float xCoor = (float(origin.X) + (x + 0.5f) * blockSize) * WorldGenerator::COOR_STEP;
+			float yCoor = (float(origin.Y) + (y + 0.5f) * blockSize) * WorldGenerator::COOR_STEP;
+			WorldGenerator::Biomes biomes;
+			int height = WorldGenerator::getRealHeightAndBiomes(xCoor, yCoor, biomes);
 			for (int z = 0; z < depth; z++)
 			{
-				BlockEnumType type;
-				if (z < height)
-				{
-					type = BlockEnumType::STONE;
-				}
-				else if (z == height)
-				{
-					type = BlockEnumType::GRASS;
-				}
-				else
-				{
-					type = BlockEnumType::AIR;
-				}
+				BlockEnumType type = WorldGenerator::getBlockType(x, y, height, biomes, z);
 				Blocks[x][y][z] = type;
 			}
 		}
@@ -62,17 +55,17 @@ void Chunk::GenerateTerrain()
 
 void Chunk::RenderAllBlocks(BlockInstancesManager* manager)
 {
-	for (int x=0; x<size; x++)
+	for (int x = 0; x < size; x++)
 	{
-		for (int y =0; y<size; y++)
+		for (int y = 0; y < size; y++)
 		{
-			for (int z=0; z<depth; z++)
+			for (int z = 0; z < depth; z++)
 			{
 				if (BlocksVisibleStatus[x][y][z])
 				{
-					FVector location(blockSize*x,blockSize*y,blockSize*(z));
+					FVector location(blockSize * x, blockSize * y, blockSize * (z));
 					FTransform transform;
-					transform.SetLocation(location+origin);
+					transform.SetLocation(location + origin);
 					BlocksInstanceIndex[x][y][z] = manager->AddBlockInstance(Blocks[x][y][z], transform);
 				}
 			}
@@ -97,8 +90,13 @@ void Chunk::CheckTerrainBlockVisibility()
 		{
 			for (int z = 0; z < depth; z++)
 			{
-				if (Blocks[x][y][z]==BlockEnumType::AIR)
+				if (Blocks[x][y][z] == BlockEnumType::AIR)
 				{
+					continue;
+				}
+				if (x==0 || x==size-1 ||y==0||y==size-1||z==0||z==depth-1)
+				{
+					BlocksVisibleStatus[x][y][z] = true;
 					continue;
 				}
 				if (CheckIsType(x - 1, y, z, BlockEnumType::AIR)
--- Minecraft/Source/Minecraft/World/WorldConstants.cpp
+++ Minecraft/Source/Minecraft/World/WorldConstants.cpp
@@ -2,8 +2,8 @@
 
 namespace WorldConstants
 {
-	const uint32 RENDER_RANGE = 10;
-	const uint8 CHUNK_ELEMENT_COUNT = 30;
+	const uint32 RENDER_RANGE = 5;
+	const uint8 CHUNK_ELEMENT_COUNT = 16;
 	const uint16 CHUNK_DEPTH = 256;
 	const uint8 UNIT_BLOCK_SIZE = 100;
 }
